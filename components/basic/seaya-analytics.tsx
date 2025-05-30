4e9d2ba8f187a584a5f6af839c8f4ff279c916d3
Seameee <overnightneko@gmail.com>
2025-05-30 19:52:12 +0800

    🐛 fix(analytics): 更新分析脚本域名
    - 将分析脚本域名从analytics.seaya.link更新为ayano.seaya.link

--- components/basic/seaya-analytics.tsx
+++ components/basic/seaya-analytics.tsx
@@ -3,13 +3,8 @@
 export default function SeayaAnalytics() {
   return (
     <script
-<<<<<<< HEAD
       src="https://ayano.seaya.link/script.js"
       data-website-id="859b0512-dbee-4c15-b866-dd8481b6d56a"
-=======
-      src="https://analytics.seaya.link/api/script.js"
-      data-site-id="2"
->>>>>>> e1fb4d1 (✨ feat(analytics): 优化分析脚本配置)
       crossOrigin="anonymous"
       defer
       dangerouslySetInnerHTML={{__html: ''}}
