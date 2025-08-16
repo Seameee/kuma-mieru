895ede8036af5d8099b34dfbfa2ba24fb757d631
Seamee <31821836+Seameee@users.noreply.github.com>
2025-08-16 11:13:52 +0800

    Update seaya-analytics.tsx

--- components/basic/seaya-analytics.tsx
+++ components/basic/seaya-analytics.tsx
@@ -3,8 +3,8 @@
 export default function SeayaAnalytics() {
   return (
     <script
-      src="https://ayano.seaya.link/api/script.js"
-      data-site-id="2"
+      src="https://ayano.seaya.link/script.js"
+      data-website-id="859b0512-dbee-4c15-b866-dd8481b6d56a"
       crossOrigin="anonymous"
       defer
       dangerouslySetInnerHTML={{__html: ''}}
